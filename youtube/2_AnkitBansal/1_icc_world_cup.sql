87883be98e3b64d76f9e331d572571ec2ea520dc
Sushovan Karmakar <sushovankarmakar1996@gmail.com>
2025-03-18 09:11:39 +0530

    ankit_bansal : q1 : better approach , Reduced table scans from 2 to 1

--- youtube/2_AnkitBansal/1_icc_world_cup.sql
+++ youtube/2_AnkitBansal/1_icc_world_cup.sql
@@ -1,26 +1,27 @@
 
-CREATE TABLE icc_world_cup
-(
+CREATE TABLE icc_world_cup (
 	team1 varchar(20),
 	team2 varchar(20),
 	winner varchar(20)
 );
 
 INSERT INTO icc_world_cup
-(team1, team2, winner)
+	(team1, team2, winner)
 VALUES
-('Ind', 'SL', 'Ind'),
-('SL', 'Aus', 'Aus'),
-('SA', 'Eng', 'Eng'),
-('Eng', 'NZ', 'NZ'),
-('Aus', 'Ind', 'Ind');
+	('Ind', 'SL', 'Ind'),
+	('SL', 'Aus', 'Aus'),
+	('SA', 'Eng', 'Eng'),
+	('Eng', 'NZ', 'NZ'),
+	('Aus', 'Ind', 'Ind');
 
 SELECT * FROM icc_world_cup iwc ;
 
-SELECT team_name, 
-count(*) AS matches_played, 
-sum(win_flag) AS num_of_wins, 
-count(*) - sum(win_flag) AS num_of_losses 
+-- approach 1
+SELECT 
+	team_name, 
+	count(*) AS matches_played, 
+	sum(win_flag) AS num_of_wins, 
+	count(*) - sum(win_flag) AS num_of_losses 
 FROM
 (
 	(SELECT iwc.team1 AS team_name,
@@ -39,4 +40,22 @@ FROM
 ) AS win_flag_table
 GROUP BY win_flag_table.team_name
 ORDER BY num_of_wins DESC ;
+
+-- approach 2 : better,  Unpivot teams into single column, Reduced table scans from 2 to 1 
+WITH winners_table AS (
+	-- Unpivot teams into single column
+	SELECT team1 AS team, winner FROM icc_world_cup iwc
+	UNION ALL
+	SELECT team2 AS team, winner FROM icc_world_cup iwc
+)
+SELECT 
+	team,
+	count(team) AS matches_played,
+	sum(CASE WHEN team = winner THEN 1 ELSE 0 END) AS num_of_wins,
+	sum(CASE WHEN team != winner THEN 1 ELSE 0 END) AS num_of_losses
+FROM 
+	winners_table 
+GROUP BY team
+ORDER BY num_of_wins DESC ;
+	
 
